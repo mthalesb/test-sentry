f7fd6a5ba6654c4144b37f03027c35a2ce37b1e2
Thales M <mthalesb@proton.me>
2025-12-08 21:18:41 -0300

    add sentry

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,13 @@
+'use client'
+
 import Image from "next/image";
+import * as Sentry from "@sentry/nextjs";
 
 export default function Home() {
+  const test = () => {
+    Sentry.logger.info('User triggered test log', { log_source: 'sentry_test' })
+  }
+
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
@@ -57,6 +64,14 @@ export default function Home() {
             rel="noopener noreferrer"
           >
             Documentation
+          </a>
+          <a>
+            <button
+              onClick={test}
+              className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
+            >
+              Test Sentry
+            </button> 
           </a>
         </div>
       </main>
